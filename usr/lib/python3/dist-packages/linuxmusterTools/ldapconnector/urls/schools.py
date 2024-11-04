db92876bf0889835edc5fafd9fd373fa6ab57b56
kiarn <arnaud@linuxmuster.net>
2024-11-04 10:22:43 +0100

    Get specific school.

--- usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/urls/schools.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/urls/schools.py
@@ -10,3 +10,13 @@ def get_all_schools():
     """
 
     return f"""(&(objectClass=organizationalUnit))"""
+
+
+@router.single(r'/schools/(?P<school>[a-zA-Z0-9_\-äëïöüÄËÏÖÜßéàèùçÀÉÈÇÙâêîôûÂÊÛÔÎ]*)', models.LMNSchool, subdn='OU=SCHOOLS,', level="single")
+def get_specific_school(school):
+    """
+    Get a specific school.
+    Return a LMNSchool data object
+    """
+
+    return f"""(&(objectClass=organizationalUnit)(name={school}))"""
