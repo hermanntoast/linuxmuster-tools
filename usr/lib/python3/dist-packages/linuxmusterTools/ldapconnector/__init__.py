0d4a18d77ab30bf50b3012409d0ac03d4ffa9657
kiarn <arnaud@linuxmuster.net>
2024-11-11 11:25:35 +0100

    Global import from writers.

--- usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/__init__.py
+++ usr/lib/python3/dist-packages/linuxmusterTools/ldapconnector/__init__.py
@@ -1,6 +1,6 @@
 from linuxmusterTools.ldapconnector.urls import router as reader_router
 from linuxmusterTools.ldapconnector.ldap_writer import ldap_writer
-
+from linuxmusterTools.ldapconnector.writers import *
 
 LMNLdapReader = reader_router
 LMNLdapWriter = ldap_writer
